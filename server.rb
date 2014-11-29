f5bd112c07079f2704ad350e345b55a08310495c
cmkoller <cmkoller01@gmail.com>
2014-11-29 15:05:07 -0500

    Fully changed over to using relational database rather than CSV. Working except for one bug - deleted stories don't seem to be removed from consideration when not allowing duplicate story names

--- server.rb
+++ server.rb
@@ -14,75 +14,56 @@ def db_connection
   end
 end
 
-LIST_OF_STORIES = 'list_of_stories.txt'
-
-# Creates a hash of adventures with:
-# => :STORY_TITLE
-# => :AUTHOR
-# => :PAGES, a hash of PAGE_ID (integer) --> PAGE_INFO_HASH pairings;
-# =>  one for each page in the story.
-# Within each page's hash, values are:
-# => :TITLE, title of page
-# => :TEXT, the main story text to be displayed on each page
-# => :END_POINT, boolean representing if we're at the end point of the story
-# => :ACTIONS, an array of two-item arrays, each representing
-# =>  an option-text/destination-id pairing for each option
-def process_adventure(filename)
-  unprocessed_adventure = CSV.read(filename)
-  adventure = {}
-
-  meta_info = unprocessed_adventure.shift
-  adventure[:story_title] = meta_info[0]
-  adventure[:author] = meta_info[1]
-  adventure[:pages] = {}
-  unprocessed_adventure.each do |page|
-    index = page[0]
-    title = page[1]
-    text = page[2]
-    actions = page[3..-1]
-
-    adventure[:pages][index] = {}
-    current = adventure[:pages][index]
-
-    current[:title] = title
-    current[:text] = text
-    current[:end_point] = actions.empty?
-    current[:actions] = []
-
-    unless current[:end_point]
-      count = 0
-      while count <= actions.length - 1
-        current[:actions] << [actions[count], actions[count + 1]]
-        count += 2
-      end
-    end
-
-  end
-  adventure
-end
-
-
 get '/' do
   redirect '/stories'
 end
 
-get '/story/:name' do
-  redirect "/story/#{params[:name]}/1"
+get '/stories' do
+  db_connection do |connection|
+    @stories = connection.exec("SELECT * FROM stories")
+  end
+  erb :stories
 end
 
-get '/story/:name/:index' do
-  story_name = params[:name]
-  adventure = process_adventure("#{story_name}.csv")
+post '/stories' do
+  db_connection do |connection|
+    connection.exec_params("DELETE FROM stories WHERE id = $1",
+    [params[:delete]])
+  end
+  redirect '/storiesø'
+end
 
-  @index = params[:index]
+get '/story/:index' do
+  redirect "/story/#{params[:index]}/1"
+end
 
-  @story_title = adventure[:story_title]
-  @author = adventure[:author]
-  page_info = adventure[:pages][@index]
-  @title = page_info[:title]
-  @text = page_info[:text]
-  @end_point = page_info[:end_point]
-  @actions = page_info[:actions]
+get '/story/:index/:page' do
+  story_index = params[:index]
+  page = params[:page]
+
+  db_connection do |connection|
+    info = connection.exec_params("SELECT
+      FROM pages.page_header, pages.page_body, pages.action1, pages.dest1,
+      pages.action2, pages.dest2, pages.action3, pages.dest3, pages.action4,
+      pages.dest5, stories.title AS story_title, stories.author
+      JOIN pages ON stories.id = pages.story_id
+      WHERE stories.id = $1 AND pages.page_num = $2",
+    [story_index, page])
+
+    @story_title = info[0]["story_title"]
+    @author = info[0]["author"]
+    @page_header = info[0]["page_header"]
+    @text = info[0]["page_body"]
+    @actions = [info[0]["action1"], info[0]["dest1"], info[0]["action2"],
+    info[0]["dest2"], info[0]["action3"], info[0]["dest3"], info[0]["action4"],
+    info[0]["dest4"]]
+    @actions.delete(NIL)
+    if @actions.empty?
+      @end_point = true
+    else
+      @end_point = false
+    end
+  end
 
   erb :story_view
 end
@@ -122,7 +103,8 @@ post '/create/:story_id' do
   @id = params[:story_id]
   if params[:delete]
     db_connection do |connection|
-      connection.exec("DELETE FROM pages WHERE id = #{params[:delete]}")
+      connection.exec_params("DELETE FROM pages WHERE id = $1",
+      [params[:delete]])
     end
   else
     # Write the new page to the PAGES database
@@ -173,11 +155,4 @@ post '/create/:story_id' do
 
   # Redirect to display the page
   redirect "/create/#{params[:story_id]}"
-end
-
-
-
-get '/stories' do
-  @stories = File.read(LIST_OF_STORIES).split("\n")
-  erb :stories
 end
